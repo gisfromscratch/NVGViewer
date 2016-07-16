69541cd3015e6c78eb199a49219e1f73d276d9c0
J.T <gisfromscratch@live.de>
2016-07-16 16:24:16 +0200

    #1: Created an ArcGIS Runtime desktop application which uses mapapps metro UI window.

---
+++ NVGViewer/NVGViewer/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Esri.ArcGISRuntime.Controls;
+using MahApps.Metro.Controls;
+using System.Diagnostics;
+
+namespace NVGViewer
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FocusMapViewLayerLoaded(object sender, LayerLoadedEventArgs e)
+        {
+            if (e.LoadError == null)
+                return;
+
+            Debug.WriteLine(string.Format("Error while loading layer : {0} - {1}", e.Layer.ID, e.LoadError.Message));
+        }
+    }
+}
